f06242b216f7ab506929f779816dc404cd11da32
Almehwari <MOHWRYAA@PETRORABIGH.COM>
2026-07-05 23:32:36 +0300

    Add files via upload

---
+++ safetyQuestions.js
@@ -0,0 +1,666 @@
+const safetyQuestions = [
+
+{
+    question: "What type of work permit is required for brush painting?",
+    options: [
+        "Hot",
+        "Cold",
+        "Entry",
+        "Release"
+    ],
+    correct: 1
+},
+
+{
+    question: "What kind of permit is required for crane lift operation?",
+    options: [
+        "Hot work permit",
+        "Cold work permit",
+        "Release work permit",
+        "No work permit is necessary for this job"
+    ],
+    correct: 0
+},
+
+{
+    question: "What is the PRC phone number to report an emergency?",
+    options: [
+        "911",
+        "991",
+        "110",
+        "112"
+    ],
+    correct: 0
+},
+
+{
+    question: "What must happen if the standby man has to leave the area while workers are inside a confined space?",
+    options: [
+        "An alarm system must be available",
+        "The work permit must be continued",
+        "Stop the work and let workers get out of the confined space",
+        "At least two workers must work together"
+    ],
+    correct: 2
+},
+
+{
+    question: "What should you do to remove a damaged pressure gauge from service?",
+    options: [
+        "Pull out and pressure",
+        "Replace directly",
+        "Isolate and depressurize",
+        "Isolate and pressurize"
+    ],
+    correct: 2
+},
+
+{
+    question: "What is the color code of the padlock used by MFT operation for LOTO purposes?",
+    options: [
+        "Yellow",
+        "Red",
+        "Green",
+        "Blue"
+    ],
+    correct: 0
+},
+
+{
+    question: "What is the color code of the padlock used by Electrical System Operator (ESO) for LOTO purposes?",
+    options: [
+        "Green",
+        "Blue",
+        "Red",
+        "Purple"
+    ],
+    correct: 2
+},
+
+{
+    question: "What must workers do while working in a high noise area?",
+    options: [
+        "Have a radio with them",
+        "Install sound insulation",
+        "Shut down all noisy equipment",
+        "Wear ear plugs or ear muffs"
+    ],
+    correct: 3
+},
+
+{
+    question: "Among others, which PPE from the below list should be worn for the task of collecting a sample?",
+    options: [
+        "Rubber gloves",
+        "Safety shoes, safety helmet, rubber gloves and eye protection",
+        "Eye protection",
+        "Safety shoes and safety helmet"
+    ],
+    correct: 1
+},
+
+{
+    question: "Who shall conduct a gas test before issuing a work permit?",
+    options: [
+        "Anyone from MFT operation",
+        "Operator only",
+        "Certified gas tester only",
+        "SSV only"
+    ],
+    correct: 2
+},
+
+{
+    question: "What does color blue in the gas and fire system indicate?",
+    options: [
+        "State of toxic gas release",
+        "State of fire",
+        "State of unit shutdown",
+        "All"
+    ],
+    correct: 0
+},
+
+{
+    question: "As part of LOTO requirement, which organization is the first to install a lock and tag on a piece of equipment and the last to remove them?",
+    options: [
+        "Maintenance Organization",
+        "Proponent Organization",
+        "Electrical Organization",
+        "Contractor Supervisor at the job site"
+    ],
+    correct: 1
+},
+
+
+{
+    question: "As per Petro Rabigh traffic safety program, the Driving Improvement Refresher Course (DIRC) shall be attended every ___ by all PR employees, dependents (licensed drivers), and long-term contractor drivers.",
+    options: [
+        "Three years",
+        "Five years",
+        "Four years",
+        "One year"
+    ],
+    correct: 0
+},
+
+{
+    question: "What does MSDS stand for?",
+    options: [
+        "Material Safety Density Sheet",
+        "Material Security Data Sheet",
+        "Material Safety Data Sheet",
+        "Material Sample Data Sheet"
+    ],
+    correct: 2
+},
+
+{
+    question: "All sewers within ___ distance shall be covered prior to hot work in the plant areas.",
+    options: [
+        "22.5 m (75 ft)",
+        "895 m (280 ft)",
+        "15 m (50 ft)",
+        "50 m (166 ft)"
+    ],
+    correct: 0
+},
+
+{
+    question: "When climbing up or down any ladder, face the ladder and maintain ___ point(s) of contact with hands free of materials.",
+    options: [
+        "One",
+        "Two",
+        "Three",
+        "Any"
+    ],
+    correct: 2
+},
+
+{
+    question: "Each liquid chemical has specific properties and uses. It is important to read and understand the labels and the ___.",
+    options: [
+        "Material Safety Data Sheet (MSDS)",
+        "OSHA",
+        "Signs",
+        "Container size"
+    ],
+    correct: 0
+},
+
+{
+    question: "Scaffold inspection tags shall be inspected and renewed every ___.",
+    options: [
+        "4 weeks",
+        "1 week",
+        "3 weeks",
+        "2 weeks"
+    ],
+    correct: 3
+},
+
+{
+    question: "Which of the following is considered an ignition source?",
+    options: [
+        "Running engine/vehicle",
+        "Non-intrinsically safe equipment, mobile phone",
+        "Spark or flame producing tools",
+        "All of the above"
+    ],
+    correct: 3
+},
+
+{
+    question: "Locations where fire or explosion hazards may exist due to the presence of flammable gas or vapors, flammable liquids, combustible dust, etc. are known as ___.",
+    options: [
+        "Community areas",
+        "Hazardous (Classified) areas",
+        "Open areas",
+        "Non-restricted areas"
+    ],
+    correct: 1
+},
+
+{
+    question: "In Petro Rabigh, ordinary mobile phones are not permitted to be carried (even in switch-off mode) within the ___.",
+    options: [
+        "Open area",
+        "Hazardous (Classified) areas",
+        "Non-restricted areas",
+        "Community areas"
+    ],
+    correct: 1
+},
+
+{
+    question: "Inside Petro Rabigh restricted areas, smoking is ___.",
+    options: [
+        "Not permitted at all",
+        "Permitted only in approved smoking shelters/designated smoking areas",
+        "Permitted inside Process Interface Buildings (PIBs) only",
+        "Permitted inside Buildings (OBs) only"
+    ],
+    correct: 1
+},
+
+{
+    question: "What type of PPE must be used in areas with a high noise sign board?",
+    options: [
+        "SCBA",
+        "Goggles",
+        "Ear muffs or ear plugs",
+        "Face shield"
+    ],
+    correct: 2
+},
+
+{
+    question: "Approved fall protection measures (e.g., safety harnesses or scaffolding) shall be used if the working height is ___.",
+    options: [
+        "Greater than 1.2 m (4 ft)",
+        "Greater than 0.8 m (5 ft)",
+        "Greater than 1.8 m (6 ft)",
+        "At any height"
+    ],
+    correct: 2
+},
+
+{
+    question: "___ is very useful to identify and indicate the location of a fire, leak, or other emergency so that responding by the emergency services becomes easier and quicker.",
+    options: [
+        "Emergency gas leak",
+        "Emergency General Locator",
+        "Emergency Grid Location",
+        "Emergency Group Leader"
+    ],
+    correct: 2
+},
+
+{
+    question: "How should you report an emergency case?",
+    options: [
+        "Dial 911 from a Petro Rabigh landline",
+        "Dial (012) 425-0911 from mobile",
+        "Dial 911 or #1 from the bravo",
+        "Any of the above"
+    ],
+    correct: 3
+},
+
+{
+    question: "Why is H2S so dangerous?",
+    options: [
+        "H2S is highly toxic",
+        "H2S is flammable and explosive",
+        "H2S is a colorless gas, heavier than air",
+        "All the above"
+    ],
+    correct: 3
+},
+
+{
+    question: "___ indicates that the scaffold is complete, has been inspected, and is safe for use at the time of inspection.",
+    options: [
+        "White scaffold tag",
+        "Red scaffold tag",
+        "Green scaffold tag",
+        "Yellow scaffold tag"
+    ],
+    correct: 2
+},
+
+{
+    question: "A green scaffold tag is valid for a maximum period of ___.",
+    options: [
+        "One week only",
+        "One month only",
+        "Two weeks only",
+        "One day only"
+    ],
+    correct: 2
+},
+
+{
+    question: "Which of the following types of work permits are implemented in Petro Rabigh?",
+    options: [
+        "(A) Equipment Opening/Line Break, (B) Hot Work, (C) Cold Work, (D) Excavation",
+        "(A) Equipment Opening/Line Break, (B) Hot Work, (C) Cold Work, (D) Confined Space/Entry",
+        "(A) Equipment Opening/Line Break, (B) Hot Work, (C) Cold Work, (D) Radiography",
+        "(A) Equipment Opening/Line Break, (B) Hot Work, (C) Cold Work, (D) Vehicle Enter"
+    ],
+    correct: 1
+},
+
+{
+    question: "Work permit certification is valid for?",
+    options: [
+        "4 years",
+        "Two years",
+        "3 years",
+        "5 years"
+    ],
+    correct: 1
+},
+
+{
+    question: "The concentration of H2S in air is expressed in PPM. What is meant by PPM?",
+    options: [
+        "Parts Per Billion",
+        "Parts Per Million",
+        "Parts Per Measurement",
+        "Parts of Process Measurement"
+    ],
+    correct: 1
+},
+
+{
+    question: "What is LEL?",
+    options: [
+        "Lower Fuel Mixture",
+        "Lower Explosive Limit",
+        "Upper Explosive Limit",
+        "Lower Energy Limit"
+    ],
+    correct: 1
+},
+
+{
+    question: "What first aid should be given to a caustic-affected person in case of eye contact?",
+    options: [
+        "Give a large quantity of drinking water",
+        "Flush the affected part with plenty of water for more than 15 minutes",
+        "Take the affected person into fresh air",
+        "All"
+    ],
+    correct: 1
+},
+
+{
+    question: "A continuous sounding of the emergency siren for one minute duration indicates ___.",
+    options: [
+        "Declaration of \"FIRE\" emergency",
+        "Declaration of \"ALL CLEAR\" after the emergency is brought under control",
+        "Declaration of \"GAS LEAK\" emergency",
+        "Declaration of \"RESCUE\" emergency"
+    ],
+    correct: 1
+},
+
+{
+    question: "Heat exposure can be caused by:",
+    options: [
+        "Temperature and humidity",
+        "Direct sun",
+        "Hot surfaces",
+        "All of the above"
+    ],
+    correct: 3
+},
+
+{
+    question: "What can you do to control heat exposure?",
+    options: [
+        "Work as fast and hard as you can to get the job over with",
+        "Take periodic rest breaks in a cool, shaded area and drink small quantities of water frequently",
+        "Schedule the heaviest job during the hottest time of the day",
+        "All of the above"
+    ],
+    correct: 1
+},
+
+{
+    question: "The minimum requirements to drive a Petro Rabigh vehicle include:",
+    options: [
+        "Current DIP certification",
+        "Valid driving license recognized by the Saudi Arabia government",
+        "Sign the Pre-Driving Safety Undertaking",
+        "All of the above"
+    ],
+    correct: 3
+},
+
+{
+    question: "A protective cap is supplied with each compressed gas cylinder to ___.",
+    options: [
+        "Keep the valve clean",
+        "Protect the valve when the cylinder is not in use",
+        "Make it easier to use the cylinder",
+        "All of the above"
+    ],
+    correct: 1
+},
+
+{
+    question: "Which of the following is a near miss incident?",
+    options: [
+        "A hammer fell from an elevated platform and hit the worker, causing first aid injury",
+        "A hammer fell from an elevated platform but missed the worker",
+        "A hammer lying unattended at the edge of an elevated platform",
+        "All of the above"
+    ],
+    correct: 1
+},
+
+{
+    question: "___ is NOT an example of isolation for electrical equipment.",
+    options: [
+        "Open breaker",
+        "Remove fuses",
+        "Closing a valve in a process fluid pipe",
+        "Disconnect wiring"
+    ],
+    correct: 2
+},
+
+{
+    question: "For an initial work permit into any work where H2S concentration is 50 ppm, the permit must be countersigned by ___.",
+    options: [
+        "Work permit issuer",
+        "Proponent SH or PRSS during off hours",
+        "Maintenance Coordinator",
+        "Area SSV"
+    ],
+    correct: 1
+},
+
+{
+    question: "The minimum acceptable oxygen concentration for making a confined space entry is ___.",
+    options: [
+        "22%",
+        "20%",
+        "18.50%",
+        "16%"
+    ],
+    correct: 1
+},
+
+{
+    question: "Before visiting the plant areas, a visitor must ___.",
+    options: [
+        "Wear proper PPE such as helmet, safety glasses, flame-resistant clothing, and safety shoes",
+        "Wear a visitor card at all times during the visit in the plant areas",
+        "Log their name in the visitor logbook",
+        "All the above"
+    ],
+    correct: 3
+},
+
+{
+    question: "During an emergency, a \"head count\" is done at the emergency assembly area to ___.",
+    options: [
+        "Allow the maintenance crew to return to work at the emergency-affected area",
+        "Ensure that there are no missing persons in the affected area",
+        "Communicate the numbers in a safety meeting",
+        "Communicate the numbers during Tool Box Talks with contractor"
+    ],
+    correct: 1
+},
+
+{
+    question: "Upon hearing a gas leak alarm, you have to leave the area and walk in ___ to reach the nearest assembly point.",
+    options: [
+        "A down-wind direction",
+        "An up-wind direction",
+        "Any direction",
+        "A straight line"
+    ],
+    correct: 1
+},
+
+{
+    question: "The range of flammability between the Lower Explosive Limit and Upper Explosive Limit is known as?",
+    options: [
+        "Operating range",
+        "Firing range",
+        "Hazardous mixture",
+        "Safety range"
+    ],
+    correct: 1
+},
+
+{
+    question: "In the context of the PRC work permit system confined space entry requirement, an oxygen-deficient atmosphere is ___.",
+    options: [
+        "An atmosphere with less than 51% oxygen by volume at sea level",
+        "An atmosphere with less than 20% oxygen by volume at sea level",
+        "An atmosphere with less than 10% oxygen by volume at sea level",
+        "An atmosphere with less than 16% oxygen by volume at sea level"
+    ],
+    correct: 1
+},
+
+{
+    question: "During normal working hours, the confined space entry permit for the initial entry into the confined space will be countersigned by:",
+    options: [
+        "The field operator of the issuing section",
+        "Section head",
+        "The SSV of the issuing section",
+        "The PRSS"
+    ],
+    correct: 1
+},
+
+{
+    question: "During confined space work, if the standby man leaves the place and there is no reliever standby man for replacement, then ___.",
+    options: [
+        "All the entrants must stop the work and stay inside the confined space",
+        "All the entrants must exit from the confined space",
+        "He should carry the \"Log of Entrants\" with him",
+        "Inform all entrants to stay inside the confined space until he returns"
+    ],
+    correct: 1
+},
+
+{
+    question: "\"Continuous Gas Testing\" is ___ during any confined space entry, as per Petro Rabigh established procedure for confined space.",
+    options: [
+        "Preferred",
+        "Must",
+        "Desired",
+        "Suggested"
+    ],
+    correct: 1
+},
+
+{
+    question: "Which of the following is correct with respect to reporting of injury incidents in Petro Rabigh?",
+    options: [
+        "Only medical treatment injury incidents are to be reported",
+        "All injury incidents (first aid, medical treatment & lost time injury) must be reported",
+        "Only first aid injury incidents are to be reported",
+        "Only lost time injury (LTI) incidents are to be reported"
+    ],
+    correct: 1
+},
+
+
+{
+    question: "LERP is the abbreviation of:",
+    options: [
+        "Lower Emergency Responsible Program",
+        "Local Emergency Response Plan",
+        "Local Emergency Review Plan",
+        "Location Enabled Response Plan"
+    ],
+    correct: 1
+},
+
+{
+    question: "___ indicates that the scaffold has not been inspected or is not safe for use (by anyone other than scaffold craftsmen).",
+    options: [
+        "Yellow scaffold tag",
+        "Red scaffold tag",
+        "Green scaffold tag",
+        "White scaffold tag"
+    ],
+    correct: 1
+},
+
+{
+    question: "___ indicates that the scaffold is completed, has been inspected, and is safe for use at the time of inspection.",
+    options: [
+        "Yellow scaffold tag",
+        "Green scaffold tag",
+        "Red scaffold tag",
+        "White scaffold tag"
+    ],
+    correct: 1
+},
+
+{
+    question: "___ indicates that the scaffold has been inspected and may be used only by workers wearing a properly anchored personal fall arrest system, including full body harness and lanyard.",
+    options: [
+        "Green scaffold tag",
+        "Yellow scaffold tag",
+        "Red scaffold tag",
+        "White scaffold tag"
+    ],
+    correct: 1
+},
+
+{
+    question: "As per Petro Rabigh Work Permit Procedure, no work is permitted (even with a breathing apparatus set) if the hydrogen sulphide (H2S) in the equipment/air at the worksite ___.",
+    options: [
+        "Exceeds 10 PPM",
+        "Exceeds 100 PPM",
+        "Exceeds 50 PPM",
+        "Exceeds 0 (zero) PPM"
+    ],
+    correct: 1
+},
+
+{
+    question: "As per Petro Rabigh Work Permit Procedure, no work is permitted if the oxygen reading ___.",
+    options: [
+        "20% by volume",
+        "Exceeds 23.5% by volume",
+        "21–23 PPM",
+        "Exceeds 23.5 ppm"
+    ],
+    correct: 1
+},
+
+{
+    question: "While issuing an Equipment Opening/Line Break Permit (Yellow Permit), which of the following should be remembered?",
+    options: [
+        "Hot work permits may also be issued for works in the same area, at the same time",
+        "There are NO open hot work permits issued for the same job or for jobs in the same location",
+        "Hot work permits and cold work permits may also be issued for works in the same area, at the same time",
+        "None of the above"
+    ],
+    correct: 1
+},
+
+{
+    question: "What type of fire extinguisher is not recommended for use in an enclosed/confined space?",
+    options: [
+        "Dry chemical powder fire extinguisher",
+        "CO2 fire extinguisher",
+        "Water fire extinguisher",
+        "Both 2 & 3"
+    ],
+    correct: 3
+}
+
+];
+
